45eac2be122f13468bcd89b5d4d94e436063e418
Alina Mazur <amazur@e1r9p9.unit.ua>
2020-08-08 19:23:06 +0300

    beautifyed cube

--- mx_cube.c
+++ mx_cube.c
@@ -3,40 +3,44 @@ void mx_printchar(char c);
 static void edge_lines(int lvl, int diagonal, int horizontal, int max_h) {
     if (lvl == 1 || lvl == diagonal + 2 || lvl == max_h) {
         if (lvl == 1) {
-            for (int spaces = 0; spaces < diagonal + 1; spaces++)
+            for (int col = 0; col < diagonal + 1; col++) 
                 mx_printchar(' ');
         }
-        for (int i = 0; i <= horizontal + 1; i++) {
-            if (i == 0 || i == horizontal + 1)
+        for (int col = 0; col <= horizontal + 1; col++) {
+            if (col == 0 || col == horizontal + 1)
                 mx_printchar('+');
             else
                 mx_printchar('-');
         }
         if (lvl == diagonal + 2) {
-            for (int spaces = 0; spaces < lvl - 2; spaces++) 
+            for (int col = 0; col < lvl - 2; col++) 
                 mx_printchar(' ');
             mx_printchar('|');
         }
         mx_printchar('\n');
     }
 }
+
 static void top_panel(int lvl, int diagonal, int horizontal) {
     if (lvl > 1 && lvl <= diagonal + 1) {
-        for (int spaces = 0; spaces < diagonal - lvl + 2; spaces++)
+        for (int col = 0; col < diagonal - lvl + 2; col++) 
             mx_printchar(' ');
         mx_printchar('/');
-        for (int i = 0; i < horizontal; i++) mx_printchar(' ');
+        for (int col = 0; col < horizontal; col++) 
+            mx_printchar(' ');
         mx_printchar('/');
-        for (int spaces = 0; spaces < lvl - 2; spaces++) 
+        for (int col = 0; col < lvl - 2; col++)
             mx_printchar(' ');
         mx_printchar('|');
         mx_printchar('\n');
     }
 }
+
 static void front_block(int lvl, int diagonal, int horizontal, int max_h) {
     if (lvl > diagonal + 2 && lvl < max_h - diagonal - 1) {
-        for (int i = 0; i <= horizontal + diagonal + 2; i++) {
-            if (i == 0 || i == horizontal + 1 || i == horizontal + diagonal + 2)
+        for (int col = 0; col <= horizontal + diagonal + 2; col++) {
+            if (col == 0 || col == horizontal + 1 ||
+                col == horizontal + diagonal + 2)
                 mx_printchar('|');
             else
                 mx_printchar(' ');
@@ -44,15 +48,16 @@ static void front_block(int lvl, int diagonal, int horizontal, int max_h) {
         mx_printchar('\n');
     }
 }
+
 static void front_corner(int lvl, int diagonal, int horizontal, int max_h) {
     if (lvl > max_h - diagonal - 2 && lvl < max_h) {
-        for (int i = 0; i <= horizontal + 1; i++) {
-            if (i == 0 || i == horizontal + 1)
+        for (int col = 0; col <= horizontal + 1; col++) {
+            if (col == 0 || col == horizontal + 1)
                 mx_printchar('|');
             else
                 mx_printchar(' ');
         }
-        for (int i = 0; i < max_h - lvl - 1; i++) 
+        for (int col = 0; col < max_h - lvl - 1; col++) 
             mx_printchar(' ');
         if (lvl == max_h - diagonal - 1)
             mx_printchar('+');
@@ -67,8 +72,7 @@ void mx_cube(int n) {
     int diagonal = n / 2;
     int max_h = n + diagonal + 3;
 
-    if (n <= 1)
-        return;
+    if (n <= 1) return;
     for (int lvl = 1; lvl <= max_h; lvl++) {
         edge_lines(lvl, diagonal, horizontal, max_h);
         top_panel(lvl, diagonal, horizontal);
